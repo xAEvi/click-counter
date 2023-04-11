222fa54fb4b3cc4df01697a3af41d4a5372869f1
xaevi <xeavi02@hotmail.com>
2023-04-11 11:22:42 -0500

    Finalización del proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import mouselogo from './images/mouselogo.png';
+import Button from './components/Button'
+import Counter from './components/Counter'
+import { useState } from 'react';
 
 function App() {
+
+  const [clickCount, setClickCount] = useState(0);
+
+  const addCounter = () => {
+    setClickCount(clickCount + 1)
+  };
+
+  const resetCounter = () => {
+    setClickCount(0)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='mouse-logo-container'>
+        <h1>Click Counter</h1>
+        <img 
+          className='mouse-logo'
+          alt='Mouse logo'
+          src={mouselogo}/>
+      </div>
+      <div className='principal-container'>
+        <Counter numClicks={clickCount} />
+        <Button 
+          text='Click'
+          isClickButton={true}
+          handleClick={addCounter}/>
+        <Button 
+          text='Reset'
+          isClickButton={false}
+          handleClick={resetCounter}/>
+      </div>
     </div>
   );
 }
